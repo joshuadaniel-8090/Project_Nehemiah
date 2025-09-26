538d1d09baf9196ca99fcf57ca48ff2737e81ce3
Microwrk <jjoshuadaniel1234@gmail.com>
2025-09-26 17:40:44 +0530

    qr 5

--- app/api/mark-attendance/route.ts
+++ app/api/mark-attendance/route.ts
@@ -1,6 +1,5 @@
+// app/api/mark-attendance/route.ts
 import { supabase } from "@/lib/supabase";
-
-export const dynamic = "force-dynamic";
 
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
